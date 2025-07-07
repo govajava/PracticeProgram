7016f05c2bb1bb7f1c49675b9b8cf371b5836dde
govajava4@gmail.com <govajava4@gmail.com>
2025-07-07 22:48:36 +0530

    String with array code

---
+++ src/main/java/com/practice/Arrays/StringWithArray.java
@@ -0,0 +1,34 @@
+package com.practice.Arrays;
+
+public class StringWithArray {
+
+	public static class StudentDetails {
+		int Sid;
+		String Sname;
+		int Smarks;
+	}
+
+	public static void main(String[] args) {
+
+		StudentDetails s1 = new StudentDetails();
+		s1.Sid = 1;
+		s1.Sname = "bc";
+		s1.Smarks = 88;
+
+		StudentDetails s2 = new StudentDetails();
+		s2.Sid = 2;
+		s2.Sname = "fb";
+		s2.Smarks = 88;
+
+		StudentDetails s3 = new StudentDetails();
+		s3.Sid = 3;
+		s3.Sname = "sd";
+		s3.Smarks = 88;
+
+		StudentDetails students[] = new StudentDetails[2];
+		students[0] = s1;
+		students[1] = s2;
+		students[2] = s3;
+
+	}
+}
