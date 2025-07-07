93a84d071c523468eff0e08e009adab8ad6e8f8c
govajava4@gmail.com <govajava4@gmail.com>
2025-07-07 23:00:53 +0530

    Swap code

---
+++ src/main/java/com/practice/Arrays/Swap.java
@@ -0,0 +1,17 @@
+package com.practice.Arrays;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a = 10;
+      int b = 20;
+      System.out.println("Before swaping:"+ "a:"+a+","+"b:"+b);
+
+      a = a+b;
+      b = a-b;
+      a = a-b;
+      System.out.println("After swaping:"+ "a:"+a+","+"b:"+b);
+	}
+
+}
