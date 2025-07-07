f3706e11576d7dbab8ccce0286579e494d68e0fd
govajava4@gmail.com <govajava4@gmail.com>
2025-07-07 22:31:28 +0530

    Array code

---
+++ src/main/java/com/practice/Arrays/Arrays.java
@@ -0,0 +1,36 @@
+package com.practice.Arrays;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int a = 3;
+		if (a < 10 && a > 6)
+			System.out.println("hello");
+		else
+			System.out.println("Byee");
+
+		// Arrays : Multiple values can store in only one variable is Array.
+		int arr[] = { 1, 2, 3, 4, 5 };
+
+		// if u know the size in advance.
+		int arr1[] = new int[4];
+		arr1[0] = 1;
+		arr1[1] = 2;
+		arr1[2] = 4;
+		arr1[3] = 4;
+
+		// to get only one value from array
+		System.out.println(arr[3]);
+
+		// to get whole array
+		for (int i = 0; i <= 3; i++) {
+			System.out.println(arr[i]);
+		}
+
+		// Enhanced For loop(instead of above for loop we use Enhanced For loop)
+		for (int n : arr1) {
+			System.out.println(n);
+		}
+
+	}
+}
