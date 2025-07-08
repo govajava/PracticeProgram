8a177ef851d271156a35cd44710159a4ed327dde
govajava4@gmail.com <govajava4@gmail.com>
2025-07-08 17:50:50 +0530

    For testing

---
+++ src/main/java/com/practice/programes/SampleCode.java
@@ -0,0 +1,11 @@
+package com.practice.programes;
+
+public class SampleCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+
+	}
+
+}
